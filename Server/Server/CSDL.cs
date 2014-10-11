8dd768322fe0264bf4bb6f217d6ff4417192fa47
THKT53 Trần Xuân Nam <fujimaru251993@gmail.com>
2014-10-12 00:01:48 +0700

    1st commit

---
+++ Server/Server/CSDL.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class CSDL
+    {
+        public static string ketnoiCSDL()
+        {
+            return "Data Source =MYPC; Initial Catalog =Chat_Data; Integrated Security = True";       
+        }
+
+    }
+}
